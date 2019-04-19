5a9cf6c9fa3747cdeeac2d13fca9d8f7fae81fc2
ita07 <iliastapeinos99@gmail.com>
2019-04-19 16:18:56 +0300

    Add styling and stage functionality

--- Hangman/src/com/application/games/Hangman/Main.java
+++ Hangman/src/com/application/games/Hangman/Main.java
@@ -2,8 +2,9 @@ package com.application.games.Hangman;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -11,9 +12,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Menu.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.initStyle(StageStyle.UNDECORATED);
+			primaryStage.setIconified(true);
+			primaryStage.setResizable(true);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
